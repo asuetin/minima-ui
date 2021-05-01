1761c40f8ea992b7e95931de8882008d3af2d34e
Alexey Suetin <alexeysuetin@outlook.com>
2021-05-01 14:53:31 +0300

    Add RadioGroup test

---
+++ src/__tests__/RadioGroup.test.tsx
@@ -0,0 +1,38 @@
+import {render, fireEvent} from '@testing-library/react';
+
+import RadioGroup from 'components/RadioGroup';
+
+const options = Array.from({length: 100}, (v, i) => ({
+	value: i,
+	label: `Option ${i+1}`
+}));
+
+describe('RadioGroup', () => {
+	test('change the value of the RadioGroup with a mouse', async () => {
+		let value = 0;
+
+		const {getByLabelText} = render(<RadioGroup
+			options={options}
+			value={value}
+			onChange={(v: number) => value = v}
+		/>);
+
+		fireEvent.click(getByLabelText('Option 3'));
+
+		expect(value).toEqual(2);
+	});
+
+	test('change the value of a combobox with a keyboard', async () => {
+		let value = 0;
+
+		const {container, getByLabelText} = render(<RadioGroup
+			options={options}
+			value={value}
+			onChange={(v: number) => value = v}
+		/>);
+
+		getByLabelText('Option 1').focus();
+		fireEvent.keyDown(container, {code: 'ArrowDown'});
+		expect(value).toEqual(1);
+	});
+});
