3dfee922a68090b79383b9ef0738a67e83c490ee
Alexey Suetin <alexeysuetin@outlook.com>
2021-05-05 15:28:58 +0300

    index.tsx: reorganize exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,39 @@
 import Icon, {IconStyles} from 'components/Icon';
-import List, {ListStyles} from 'components/List';
-import ComboBox, {ComboBoxStyles} from 'components/ComboBox';
-import Button, {ButtonStyles} from 'components/Button';
-import Switch, {SwitchStyles} from 'components/Switch';
-import CheckBox, {CheckBoxStyles} from 'components/CheckBox';
-import TextInput, {TextInputStyles} from 'components/TextInput';
-import RadioGroup, {RadioGroupStyles} from 'components/RadioGroup';
-import Slider, {SliderStyles} from 'components/Slider';
-import ProgressBar, {ProgressBarStyles} from 'components/ProgressBar';
-
 export {IconProps} from 'components/Icon';
-export {ListProps} from 'components/List';
-export {ComboBoxProps} from 'components/ComboBox';
-export {ButtonProps} from 'components/Button';
-export {SwitchProps} from 'components/Switch';
-export {CheckBoxProps} from 'components/CheckBox';
-export {TextInputProps} from 'components/TextInput';
-export {RadioGroupProps} from 'components/RadioGroup';
-export {SliderProps} from 'components/Slider';
-export {ProgressBarProps} from 'components/ProgressBar';
+export {Icon, IconStyles};
 
-export {
-	Icon, IconStyles,
-	ComboBox, ComboBoxStyles,
-	List, ListStyles,
-	Button, ButtonStyles,
-	Switch, SwitchStyles,
-	CheckBox, CheckBoxStyles,
-	TextInput, TextInputStyles,
-	RadioGroup, RadioGroupStyles,
-	Slider, SliderStyles,
-	ProgressBar, ProgressBarStyles
-};
+import List, {ListStyles} from 'components/List';
+export {ListProps} from 'components/List';
+export {ComboBox, ComboBoxStyles};
+
+import ComboBox, {ComboBoxStyles} from 'components/ComboBox';
+export {ComboBoxProps} from 'components/ComboBox';
+export {List, ListStyles};
+
+import Button, {ButtonStyles} from 'components/Button';
+export {ButtonProps} from 'components/Button';
+export {Button, ButtonStyles};
+
+import Switch, {SwitchStyles} from 'components/Switch';
+export {SwitchProps} from 'components/Switch';
+export {Switch, SwitchStyles};
+
+import CheckBox, {CheckBoxStyles} from 'components/CheckBox';
+export {CheckBoxProps} from 'components/CheckBox';
+export {CheckBox, CheckBoxStyles};
+
+import TextInput, {TextInputStyles} from 'components/TextInput';
+export {TextInputProps} from 'components/TextInput';
+export {TextInput, TextInputStyles};
+
+import RadioGroup, {RadioGroupStyles} from 'components/RadioGroup';
+export {RadioGroupProps} from 'components/RadioGroup';
+export {RadioGroup, RadioGroupStyles};
+
+import Slider, {SliderStyles} from 'components/Slider';
+export {SliderProps} from 'components/Slider';
+export {Slider, SliderStyles};
+
+import ProgressBar, {ProgressBarStyles} from 'components/ProgressBar';
+export {ProgressBarProps} from 'components/ProgressBar';
+export {ProgressBar, ProgressBarStyles};
