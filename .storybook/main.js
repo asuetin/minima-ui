6e68cba5859b8c4d18e9e6a6888f9235c2998801
Alexey Suetin <alexeysuetin@outlook.com>
2021-05-12 19:58:35 +0300

    Storybook: add theme preview addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,8 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-a11y'
+		'@storybook/addon-a11y',
+		'storybook-addon-styled-component-theme/dist/preset'
 	],
 	core: {
 		builder: 'webpack5'
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,23 @@
+import {addDecorator} from '@storybook/react';
+import {withThemesProvider} from 'storybook-addon-styled-component-theme';
+import {ThemeProvider} from 'styled-components';
+
+import {minimaLight, minimaDark, minimaPlain} from 'utils/theme';
+
+const themes = [
+	{
+		name: 'Minima Light',
+		minima: minimaLight
+	}, {
+		name: 'Minima Dark',
+		minima: minimaDark
+	}, {
+		name: 'Minima Plain',
+		minima: minimaPlain
+	}
+];
+addDecorator(withThemesProvider(themes), ThemeProvider);
+
 export const parameters = {
 	layout: 'centered',
 };
